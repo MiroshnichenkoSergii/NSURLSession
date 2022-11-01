82617e9174b7360b5f8983ac2007b1f189010415
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2022-11-01 12:36:50 +0200

    Add review for displayURL method

--- NSURLSession/ViewController.swift
+++ NSURLSession/ViewController.swift
@@ -12,30 +12,41 @@ class ViewController: UIViewController {
 
     @IBOutlet var myWebView: WKWebView!
     
+    let myURLAdress = "http://www.google.com/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         displayURL()
     }
     
-    // Try review old variant (it works but without images in load)
+    // Review for displayURL method
     func displayURL() {
-        let myURLAdress = "http://www.google.com/"
-        let myURL = NSURL(string: myURLAdress)
-        let URLTask = URLSession(configuration: .default).dataTask(with: myURL! as URL) { myData, response, error in
-
-            guard error == nil else { return }
-
-            let myHTMLString = String(data: myData!, encoding: String.Encoding.utf8)
-
-            DispatchQueue.main.async {
-                self.myWebView.loadHTMLString(myHTMLString!, baseURL: nil)
-            }
-
-            print(myHTMLString!)
-        }
-        URLTask.resume()
+        guard let myURL = URL(string: myURLAdress) else { return }
+        let myData = try! Data(contentsOf: myURL)
+        let myHTMLString = String(data: myData, encoding: String.Encoding.utf8)
+        print(myHTMLString!)
+        myWebView.load(URLRequest(url: myURL))
     }
+    
+    // Try review old variant (it works but without images in load)
+//    func displayURL() {
+//        let myURLAdress = "http://www.google.com/"
+//        let myURL = NSURL(string: myURLAdress)
+//        let URLTask = URLSession(configuration: .default).dataTask(with: myURL! as URL) { myData, response, error in
+//
+//            guard error == nil else { return }
+//
+//            let myHTMLString = String(data: myData!, encoding: String.Encoding.utf8)
+//
+//            DispatchQueue.main.async {
+//                self.myWebView.loadHTMLString(myHTMLString!, baseURL: nil)
+//            }
+//
+//            print(myHTMLString!)
+//        }
+//        URLTask.resume()
+//    }
 
 
 }
